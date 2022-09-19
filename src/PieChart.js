24d6cf29a93f976c30fbd2f69af8a3aa20af47bd
Til Hunke <til.hunke@study.hs-duesseldorf.de>
2022-09-19 17:39:07 +0200

    pieChart label update

--- src/PieChart.js
+++ src/PieChart.js
@@ -1,12 +1,8 @@
 import { Doughnut } from 'react-chartjs-2';
-import { Box, Card, CardContent, CardHeader, Divider, Typography, useTheme } from '@mui/material';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import PhoneIcon from '@mui/icons-material/Phone';
-import TabletIcon from '@mui/icons-material/Tablet';
+import { Box, useTheme } from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 
-
-
+//Funktion erstellt Kreisdiagramm
 export default function PieChart({chartColors, chartData, chartLabels, trackCategories}){
 
 //export const TrafficByDevice = (props, test) => {
@@ -49,51 +45,42 @@ export default function PieChart({chartColors, chartData, chartLabels, trackCate
     animation: false,
     cutoutPercentage: 80,
     layout: { padding: 0 },
-    legend: {
-      display: false
+    plugins:{
+      legend: {
+        display: true,
+        position: 'top',
+        labels: {
+          font: {
+            size: 14
+          },
+        },
+        onClick: (e, elements) => {
+          //Tue nichts
+        }
+      },
+      tooltip: {
+        backgroundColor: theme.palette.background.paper,
+        borderColor: theme.palette.divider,
+        borderWidth: 1,
+        enabled: true,
+        intersect: false,
+        mode: 'index',
+        callbacks: {
+          labelTextColor: function(){
+            return '#000000'
+          },
+        },
+      }
     },
     maintainAspectRatio: false,
     responsive: true,
-    /*tooltips: {
-      backgroundColor: theme.palette.background.paper,
-      bodyFontColor: theme.palette.text.secondary,
-      borderColor: theme.palette.divider,
-      borderWidth: 1,
-      enabled: true,
-      footerFontColor: theme.palette.text.secondary,
-      intersect: false,
-      mode: 'index',
-      titleFontColor: theme.palette.text.primary
-    }*/
   };
-
-
-  const devices = [
-    {
-      title: 'Desktop',
-      value: 63,
-      icon: LaptopMacIcon,
-      color: '#3F51B5'
-    },
-    {
-      title: 'Tablet',
-      value: 15,
-      icon: TabletIcon,
-      color: '#E53935'
-    },
-    {
-      title: 'Mobile',
-      value: 23,
-      icon: PhoneIcon,
-      color: '#FB8C00'
-    }
-  ];
 
   return (
     
     <Box
     sx={{
-      height: 300,
+      height: 350,
       position: 'relative'
     }}
   >
@@ -102,60 +89,5 @@ export default function PieChart({chartColors, chartData, chartLabels, trackCate
       options={options}
     />
   </Box>
-
-    /*<Card>
-      <CardHeader title='huhu' />
-      <Divider />
-      <CardContent>
-        <Box
-          sx={{
-            height: 300,
-            position: 'relative'
-          }}
-        >
-          <Doughnut
-            data={data}
-            options={options}
-          />
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pt: 2
-          }}
-        >
-          {devices.map(({
-            color,
-            icon: Icon,
-            title,
-            value
-          }) => (
-            <Box
-              key={title}
-              sx={{
-                p: 1,
-                textAlign: 'center'
-              }}
-            >
-              <Icon color="action" />
-              <Typography
-                color="textPrimary"
-                variant="body1"
-              >
-                {title}
-              </Typography>
-              <Typography
-                style={{ color }}
-                variant="h4"
-              >
-                {value}
-                %
-              </Typography>
-            </Box>
-          ))}
-        </Box>
-      </CardContent>
-    </Card>*/
   );
 };
