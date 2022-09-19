99ce04763c00b3ba36ebded3ae97730e35a317f6
Til Hunke <til.hunke@study.hs-duesseldorf.de>
2022-09-19 20:26:47 +0200

    update

--- src/DashboardCards.js
+++ src/DashboardCards.js
@@ -8,7 +8,7 @@ export default function DashboardCards({trackCategories}){
 
     return trackCategories.map(categorie =>(
         <>  
-            <Grid item xs={12} md={4} sm={6}>
+            <Grid item xs={12} md={4} sm={6} key={categorie[2]}>
                 <DashboardCard link={categorie[0]} image={categorie[1]} title={categorie[2]} description={categorie[3]}></DashboardCard>
             </Grid>
         </>
